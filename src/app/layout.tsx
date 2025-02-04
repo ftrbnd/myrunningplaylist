201cc8b657b38f97ef87ed23b7d39c55d2bdc818
ftrbnd <giosalas25@gmail.com>
2025-02-04 11:50:11 -0800

    Add theme provider and toggle component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -23,12 +24,22 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html
+			lang='en'
+			suppressHydrationWarning
+			// TODO: https://github.com/shadcn-ui/ui/issues/5552
+		>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<div className='flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10'>
-					{children}
-				</div>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange>
+					<div className='flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10'>
+						{children}
+					</div>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
