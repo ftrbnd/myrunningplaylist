331b68f2152fbb7d93e65ccad7692d5337227c20
ftrbnd <giosalas25@gmail.com>
2025-02-05 14:48:25 -0800

    Add avatar to logout button

--- src/components/auth/logout.tsx
+++ src/components/auth/logout.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { authClient } from '@/lib/auth-client';
-import { Button } from '../ui/button';
 import { useRouter } from 'next/navigation';
+import { authClient } from '@/lib/auth-client';
+import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+const { useSession, signOut } = authClient;
 
 export default function Logout() {
 	const router = useRouter();
+	const { data } = useSession();
 
 	const handleLogout = async () => {
-		await authClient.signOut({
+		await signOut({
 			fetchOptions: {
 				onSuccess: () => {
 					router.push('/login');
@@ -17,10 +21,22 @@ export default function Logout() {
 		});
 	};
 
+	if (!data?.session) return null;
+
 	return (
 		<Button
 			onClick={handleLogout}
 			variant='outline'>
+			<Avatar className='h-6 w-6'>
+				{data?.user.image ? (
+					<AvatarImage
+						src={data.user.image}
+						alt={data.user.name}
+					/>
+				) : (
+					<AvatarFallback>{data?.user.name[0].toUpperCase()}</AvatarFallback>
+				)}
+			</Avatar>
 			Log out
 		</Button>
 	);
