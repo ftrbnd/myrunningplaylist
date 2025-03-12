4be781a84bf7bfe3a369637c50667425f6709aac
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2025-03-12 16:45:12 -0700

    Implement session cookies

--- src/db/api.ts
+++ src/db/api.ts
@@ -6,6 +6,8 @@ import {
 	encodeHexLowerCase,
 } from '@oslojs/encoding';
 import { sha256 } from '@oslojs/crypto/sha2';
+import { cache } from 'react';
+import { cookies } from 'next/headers';
 
 export function generateSessionToken(): string {
 	const bytes = new Uint8Array(20);
@@ -57,9 +59,43 @@ export async function validateSessionToken(
 	return { session, user };
 }
 
+export async function setSessionTokenCookie(
+	token: string,
+	expiresAt: Date
+): Promise<void> {
+	(await cookies()).set('session', token, {
+		httpOnly: true,
+		sameSite: 'lax',
+		secure: process.env.NODE_ENV === 'production',
+		expires: expiresAt,
+		path: '/',
+	});
+}
+
+export async function deleteSessionTokenCookie(): Promise<void> {
+	(await cookies()).set('session', '', {
+		httpOnly: true,
+		sameSite: 'lax',
+		secure: process.env.NODE_ENV === 'production',
+		maxAge: 0,
+		path: '/',
+	});
+}
+
 export async function invalidateSession(sessionId: string): Promise<void> {
 	await db.delete(sessions).where(eq(sessions.id, sessionId));
 }
+
+export const getCurrentSession = cache(
+	async (): Promise<SessionValidationResult> => {
+		const token = (await cookies()).get('session')?.value ?? null;
+		if (token === null) {
+			return { session: null, user: null };
+		}
+		const result = await validateSessionToken(token);
+		return result;
+	}
+);
 
 export type SessionValidationResult =
 	| { session: Session; user: User }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,45 @@
-import { betterFetch } from '@better-fetch/fetch';
-import { Session } from 'better-auth/types';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-	const { data: session } = await betterFetch<Session>(
-		'/api/auth/get-session',
-		{
-			baseURL: request.nextUrl.origin,
-			headers: {
-				//get the cookie from the request
-				cookie: request.headers.get('cookie') || '',
-			},
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+	// session token cookie
+	if (request.method === 'GET') {
+		const response = NextResponse.next();
+		const token = request.cookies.get('session')?.value ?? null;
+		if (token !== null) {
+			// Only extend cookie expiration on GET requests since we can be sure
+			// a new session wasn't set when handling the request.
+			response.cookies.set('session', token, {
+				path: '/',
+				maxAge: 60 * 60 * 24 * 30,
+				sameSite: 'lax',
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+			});
 		}
-	);
+		return response;
+	}
 
-	if (request.nextUrl.pathname === '/login' && session !== null) {
-		return NextResponse.redirect(new URL('/', request.url));
-	} else if (!session && request.nextUrl.pathname !== '/login') {
-		return NextResponse.redirect(new URL('/login', request.url));
+	// CSRF protection
+	const originHeader = request.headers.get('Origin');
+	// NOTE: You may need to use `X-Forwarded-Host` instead
+	const hostHeader = request.headers.get('Host');
+	if (originHeader === null || hostHeader === null) {
+		return new NextResponse(null, {
+			status: 403,
+		});
+	}
+	let origin: URL;
+	try {
+		origin = new URL(originHeader);
+	} catch {
+		return new NextResponse(null, {
+			status: 403,
+		});
+	}
+	if (origin.host !== hostHeader) {
+		return new NextResponse(null, {
+			status: 403,
+		});
 	}
 	return NextResponse.next();
 }
-
-export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
